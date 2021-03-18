2f1fe7b2af70636af486eaee01ea91c031c4f598
Mark C <markc@localhost.localdomain>
2021-03-18 10:16:53 +0000

    fixed a few thigns, updated the model and images

--- Arduino_CurrentSense_ML/arduino_output_handler.cpp
+++ Arduino_CurrentSense_ML/arduino_output_handler.cpp
@@ -24,6 +24,9 @@ int led = LED_BUILTIN;
 // Track whether the function has run at least once
 bool initialized = false;
 
+// Track previous inference outputs...
+bool prev_out[3];
+
 // Animates a dot across the screen to represent the current x and y values
 void HandleOutput(tflite::ErrorReporter* error_reporter, float x_1, float x_2) {
   // Do this only once
@@ -32,7 +35,13 @@ void HandleOutput(tflite::ErrorReporter* error_reporter, float x_1, float x_2) {
     pinMode(led, OUTPUT);
     initialized = true;
   }
-  bool output = (x_2 == 0) ? LOW : HIGH; // we'll just use x_2, but can add for x_1 as well... 
-  digitalWrite(led, output);
+  bool output = (x_2 == 0) ? HIGH : LOW; // we'll use x_2 as we wanna focus on 'LED ON'.
+  // if the previous outputs are the same as the current, then the LED is likely on...
+  // this is to remove noisey false outputs from the model...
+  if (prev_out[0] == prev_out[1] && prev_out[1] == output){
+    digitalWrite(led, output);
+  }
   TF_LITE_REPORT_ERROR(error_reporter, "LED guess: %d\n", output);
+  prev_out[0] = prev_out[1];
+  prev_out[1] = output;
 }
